a26543a1488de00b7f528cd16625b0881d96eb36
Wyatt Gill <101477459+wyattgill9@users.noreply.github.com>
2025-05-23 17:01:04 -0700

    Flake > Shell

---
+++ flake.nix
@@ -0,0 +1,34 @@
+{
+  description = "Zig development environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            zig
+            zls
+            gdb
+            # valgrind 
+          ];
+
+          shellHook = ''
+            echo "Zig development environment"
+            echo "Zig version: $(zig version)"
+            echo "ZLS available for LSP support"
+            
+            # Set up any additional environment variables
+            # export ZIG_GLOBAL_CACHE_DIR="$PWD/.zig-cache"
+          '';
+        };
+
+      });
+}
