004ed042bfbe57c12de13dd94a8a8d05c6d9df0e
Wyatt Gill <101477459+wyattgill9@users.noreply.github.com>
2025-05-27 22:24:16 -0700

    2 structs done

--- src/parser/itch.zig
+++
@@ -1,8 +0,0 @@
-const std = @import("std");
-const utils = @import("utils.zig");
-const ITCHMessage = @import("structs.zig").ITCHMessage;
-const structs = @import("structs.zig");
-
-pub fn parseITCHMessage(buffer: []const u8) ITCHMessage {
-    return ITCHMessage.initFromBytes(buffer); 
-}
--- src/parser/utils.zig
+++ src/parser/utils.zig
@@ -1,55 +1,68 @@
 const std = @import("std");
 
-fn readU8(bytes: []const u8, offset: usize) u8 {
+pub fn readU8(bytes: []const u8, offset: usize) u8 {
     return bytes[offset];
 }
 
-fn readU16(bytes: []const u8, offset: usize) u16 {
+pub fn readU16(bytes: []const u8, offset: usize) u16 {
     return std.mem.readInt(u16, bytes[offset..offset+2][0..2], .big);
 }
 
-fn readU32(bytes: []const u8, offset: usize) u32 {
+pub fn readU32(bytes: []const u8, offset: usize) u32 {
     return std.mem.readInt(u32, bytes[offset..offset+4][0..4], .big);
 }
 
-fn readU48(bytes: []const u8, offset: usize) u48 {
+pub fn readU48(bytes: []const u8, offset: usize) u48 {
     return std.mem.readInt(u48, bytes[offset..offset+6][0..6], .big);
 }
 
-fn readU64(bytes: []const u8, offset: usize) u64 {
+pub fn readU64(bytes: []const u8, offset: usize) u64 {
     return std.mem.readInt(u64, bytes[offset..offset+8][0..8], .big);
 }
 
-fn readF8(bytes: []const u8, offset: usize) f16 {
+pub fn readF8(bytes: []const u8, offset: usize) f16 {
     const raw = readU16(bytes, offset);
     return @as(f16, @bitCast(raw));
 }
 
-fn readF32(bytes: []const u8, offset: usize) f32 {
+pub fn readF32(bytes: []const u8, offset: usize) f32 {
     const raw = readU32(bytes, offset);
     return @as(f32, @bitCast(raw));
 }
 
-fn readF64(bytes: []const u8, offset: usize) f64 {
+pub fn readF64(bytes: []const u8, offset: usize) f64 {
     const raw = readU64(bytes, offset);
     return @as(f64, @bitCast(raw));
 }
 
-fn printGenericInfo(self: anytype) void {
-    const T = @TypeOf(self);
-    const info = @typeInfo(T);
-    std.debug.print("{} {\n", .{ @typeName(T) });
+pub fn printMarketCategory(code: u8) []const u8 {
+    return switch (code) {
+        'Q' => "Nasdaq Global Select MarketSM",
+        'G' => "Nasdaq Global MarketSM",
+        'S' => "Nasdaq Capital Market®",
+        'N' => "New York Stock Exchange (NYSE)",
+        'A' => "NYSE American",
+        'P' => "NYSE Arca",
+        'Z' => "BATS Z Exchange",
+        'V' => "Investors’ Exchange, LLC",
+        ' ' => "Not available",
+        else => "Unknown Market Category Code",
+    };
+}
 
-    if (info != null and info.* == .Struct) {
-        const s = info.Struct;
-        inline for (s.fields) |field| {
-            const field_name = field.name;
-            const field_value = @field(self, field_name);
-            std.debug.print("  {s} = {any}\n", .{ field_name, field_value });
-        }
-    } else {
-        std.debug.print("  <non-struct type, cannot print fields>\n", .{});
-    }
-
-    std.debug.print("}\n", .{});
+pub fn printFinancialStatusIndicator(code: u8) []const u8 {
+    return switch (code) {
+        'D' => "Deficient",
+        'E' => "Delinquent",
+        'Q' => "Bankrupt",
+        'S' => "Suspended",
+        'G' => "Deficient and Bankrupt",
+        'H' => "Deficient and Delinquent",
+        'J' => "Delinquent and Bankrupt",
+        'K' => "Deficient, Delinquent and Bankrupt",
+        'C' => "Creations and/or Redemptions Suspended for ETP",
+        'N' => "Normal (Default): Not Deficient, Delinquent, or Bankrupt",
+        ' ' => "Not available. See SIAC feed if needed",
+        else => "Unknown Financial Status Indicator",
+    };
 }
